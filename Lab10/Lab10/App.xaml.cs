89c967129bc78e7dcb71c8c271ed72afc77804d8
gianA15 <gian.arce@tecsup.edu.pe>
2023-06-04 10:55:26 -0500

    Agregar archivos de proyecto.

---
+++ Lab10/Lab10/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Xamarin.Forms;
+
+namespace Lab10
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new ViewToViewDemo();
+            //MainPage = new BindingModeDemo();
+            //MainPage = new ListViewDemo();
+            //MainPage = new PickerDemo();
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
